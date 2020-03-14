fd2c76a972f518bdab5956e39d3a938778257c9c
S0l0kr3w <jjjunio@gmail.com>
2020-03-14 02:53:37 +0000

    Odd checker

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-
+/*
 const temperature = 8;
 
 if (temperature < 0) {
@@ -25,3 +25,20 @@ if (temperature < -40 || temperature > 40) {
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+*/
+
+const whichSchool = function (age) {
+  if(age === 0 && age <= 12)
+  return "Elementary School";
+}
+
+/*  
+if(age === 5 && age <= 13) {
+  console.log("Elementary School");
+} else if(age >= 13 && age <= 18) {
+  console.log("Secondary School");
+} else if(age > 18) {
+  console.log("Lighthouse Labs");
+}
+*/
+
